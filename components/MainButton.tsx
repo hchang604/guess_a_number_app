ffc1f88eb6047b2e392c767d8f34dc8972284f8a
hchang604 <henry.chang@alianza.com>
2022-02-27 20:26:17 -0800

    implement MainButton

---
+++ components/MainButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Colors from "../constants/colors";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+type MainButtonProps = {
+  buttonText: string;
+  onPressHandler: (parameter?: any) => void;
+};
+
+export const MainButton = (props: MainButtonProps) => {
+  return (
+    <TouchableOpacity activeOpacity={0.6} onPress={props.onPressHandler}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{props.buttonText}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: "white",
+    fontFamily: "open-sans",
+    fontSize: 18,
+    textAlign: 'center'
+  },
+});
