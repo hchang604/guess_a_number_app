1153f4cbde7ac4d16248aaf54ae42495ee281504
hchang604 <henry.chang@alianza.com>
2022-03-06 20:35:33 -0800

    implement different header styles based on platform

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 import Colors from "../constants/colors";
 
 type HeaderProps = {
@@ -8,23 +8,39 @@ type HeaderProps = {
 
 export const Header = (props: HeaderProps) => {
   return (
-    <View style={styles.headerContainer}>
+    <View
+      style={[
+        styles.headerContainerBase,
+        Platform.select({
+          ios: styles.headerContainerIOS,
+          android: styles.headerContainerAndroid
+        })
+      ]}
+    >
       <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  headerContainer: {
+  headerContainerBase: {
     width: "100%",
     height: 90,
     paddingTop: 35,
-    backgroundColor: Colors.primary,
     alignItems: "center",
     justifyContent: "center",
   },
+  headerContainerIOS: {
+    backgroundColor: "white",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  headerContainerAndroid: {
+    backgroundColor: Colors.primary,
+    borderBottomColor: "transparent",
+  },
   headerTitle: {
-    color: "black",
+    color: Platform.OS === "ios" ? Colors.primary : "white",
     fontSize: 18,
     fontFamily: "open-sans-bold",
   },
